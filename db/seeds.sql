860ad59a3441f0e59172fba674380cc058eb2a57
Taramd30 <50969601+Taramd30@users.noreply.github.com>
2019-07-23 17:26:19 -0500

    database

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO burgers
+    (
+    burger_name,
+    devoured
+    )
+VALUES
+
+    (
+        "Bacon Double Cheese Burger",
+        0
+),
+    (
+        "Inside-Out Burger",
+        0
+),
+    (
+        "Veggie Burger",
+        0
+);
